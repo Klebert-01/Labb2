b52e65420302f9eb516f09f88aca12c502f5252e
Klebert-01 <arvid.berkert@gmail.com>
2022-10-04 16:49:00 +0200

    add struktur

--- Labb2/Program.cs
+++ Labb2/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿PrintOutMenu();
+
+
+void PrintOutMenu()
+{
+    Console.WriteLine("~~~~  ARVIDS KITCHEN  ~~~~\n" +
+                      "[1] Use kitchen appliance\n" +
+                      "[2] Add new kitchen appliance\n" +
+                      "[3] List available appliances\n" +
+                      "[4] Remove kitchen appliance\n" +
+                      "[5] Leave the kitchen");
+
+}
+
+void UseAppliance()
+{
+
+}
+
+void AddNewAppliance()
+{
+
+}
+
+void ListAppliances()
+{
+
+}
+
+void RemoveAppliance()
+{
+
+}
